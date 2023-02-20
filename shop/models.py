90f9b03c72ab341622cb6572f2547e2aeaa16930
Carmel Grey <x00191659@mytudublin.ie>
2023-02-20 13:54:43 +0000

    Initialised Project, Created Category Model

---
+++ shop/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+import uuid
+
+from django.urls import reverse
+
+class Category(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False)
+    name = models.CharField(max_length=250, unique=True)
+    description = models.TextField(blank=True)
+    image = models.ImageFIeld(upload_to = 'category', blank=True)
