6feac560ebc8d83d03b8d6e3e87345ea58787be6
Carmel Grey <x00191659@mytudublin.ie>
2023-02-24 12:56:06 +0000

    doing views lab

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
 # Create your views here.
+def prod_list(request, category_id=None):
+    category =None
+    products = Product.objcets.filter(available=True)
+    if category_id:
+        category = get_object_or_404(Category, id=category_id)
+        products= Products.objects.filter(category=category, available=True)
+    return render(request, 'shop/category.html', {'category':category, 'prods':products})
